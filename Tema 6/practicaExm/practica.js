f21aaf1068399153c536be7e9c0cd2fcc67f183c
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-10-19 14:57:50 +0200

    19/Oct/2023 ---

--- Tema 6/practicaExm/practica.js
+++ Tema 6/practicaExm/practica.js
@@ -6,5 +6,22 @@ function entrada() {
          - solo temps positivas
          - mostrar medias de cada zona y media total
          [Zona[temperaturas]]
-      */
-}
+    */
+    var entrada = 0;
+    var zona = 0;
+    var terminar = false;
+    // Para las zonas
+    while (terminar) {
+        entrada = prompt("introduzca un numero");
+        // Para las temp de cada zona
+        while (true) {
+            if (entrada == "s") {
+                zona++;
+                break;
+            }
+            if (entrada == "f") {
+                return
+            }
+        }
+    }
+};
