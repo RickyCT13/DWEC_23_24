3512096abc8454c769904a05659a7f5801e98e9e
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-11-02 10:58:05 +0100

    2/11/2023 Actividad 6.2 Realizada

--- Tema 6/Actividades/02/ej2.js
+++ Tema 6/Actividades/02/ej2.js
@@ -1,29 +1,35 @@
+/**
+ * @constructor
+ * @param {Number} codigo El código del artículo.
+ * @param {String} nombre El nombre del artículo.
+ * @param {Number} precio Precio por unidad del artículo.
+ * @param {Number} cantidad Las unidades en stock.
+ * @returns {Object} El artículo generado.
+ */
 function Articulo(codigo, nombre, precio, cantidad) {
-    this.codigo = codigo,
-        this.nombre = nombre,
-        this.precio = precio,
-        this.cantidad = cantidad,
-        this.subtotal = (precio * cantidad)
-    this.mostrar = function () {
-        var datos = `
-        Codigo: ${this.codigo}
-        Nombre: ${this.nombre}
-        Precio: ${this.precio}
-        Cantidad: ${this.cantidad}
-        Subtotal: ${this.subtotal}
-        `
-        console.log(datos);
-
-    }
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
+    this.subtotal = (precio * cantidad);
 }
 
+/**
+ * @constructor
+ * @param  {...any} articulos Los artículos que se deseen añadir a la cesta en el momento de su creación.
+ * @returns {Object} La cesta vacía o con artículos ya introducidos.
+ */
 function Cesta(...articulos) {
+    /* Funcionalidad de inicializar la cesta con artículos */
     this.articulos = [];
     if (articulos !== null) {
         for (var articulo of articulos) {
             this.articulos.push(articulo);
         }
     }
+    /** Genera una tabla con los datos de cada artículo en la cesta.
+     * @method
+     */
     this.mostrar = function () {
         var cabecera = `
         <thead>
@@ -63,30 +69,98 @@ function Cesta(...articulos) {
             </tfoot>
         `;
         var contenido = cabecera + cuerpo + pie;
-        document.getElementById("cesta").innerHTML = contenido;
+        document.getElementById("tablaArticulos").innerHTML = `<table border="1">` + contenido + `</table>`;
     }
+    /** Pide los datos del nuevo artículo y lo añade a la cesta
+     * @method
+     */
     this.insertar = function () {
-        var codigo = prompt("Introduzca el código del artículo.");
+        var codigo = parseInt(prompt("Introduzca el código del artículo."));
         var nombre = prompt("Introduzca el nombre del artículo.");
         var precio = Number(prompt("Introduzca el precio por unidad del artículo."));
         var unidades = Number(prompt("Introduzca las unidades disponibles."));
         var subtotal = precio * unidades;
-        var nuevoArticulo = new Articulo(codigo, nombre, precio, unidades, subtotal);
-        this.articulos.push(nuevoArticulo);
-        this.mostrar();
+        this.articulos.push(new Articulo(codigo, nombre, precio, unidades, subtotal));
+        alert("Artículo insertado con éxito.");
     }
-    this.eliminar = function () {
-        var codigo = prompt("Introduzca el código del artículo que desea eliminar.");
+    /** Busca un artículo por código
+     * @method
+     * @param {Number} codigo El código del artículo que se busca.
+     * @returns {Number} El índice del artículo en el array interno o si no se encuentra el artículo.
+     */
+    this.buscar = function (codigo) {
         for (var articulo of this.articulos) {
             if (articulo.codigo == codigo) {
-                this.articulos.splice(this.articulos.indexOf(articulo), 1);
+                return this.articulos.indexOf(articulo);
             }
         }
-        this.mostrar();
+        return false;
     }
+
+    /** Edita un artículo de la cesta
+     * @method
+     * @param {Number} codigo El código del artículo previo a la edición.
+     */
+    this.editar = function(codigo) {
+        if (this.buscar(codigo) === false) {
+            alert("Error: Artículo no encontrado");
+            return;
+        }
+        var entrada = 0;
+        var salir = false;
+        indice = this.buscar(codigo);
+        while (salir == false) {
+            entrada = prompt(`
+            Indique el valor que desee editar:
+            1. Código.
+            2. Nombre.
+            3. Precio.
+            4. Cantidad.
+            5. Finalizar edición.
+            `);
+            switch(parseInt(entrada)) {
+                case 1:
+                    this.articulos[indice].codigo = parseInt(prompt("Introduzca el nuevo código del artículo."));
+                    break;
+                case 2:
+                    this.articulos[indice].nombre = prompt("Introduzca el nuevo nombre del artículo.");
+                    break;
+                case 3:
+                    this.articulos[indice].precio = Number(prompt("Introduzca el nuevo nombre del artículo."));
+                    break;
+                case 4:
+                    this.articulos[indice].cantidad = parseInt(prompt("Introduzca el nuevo nombre del artículo."));
+                    break;
+                case 5:
+                    salir = true;
+                    break;
+                default:
+                    alert("Error. Debe introducir una opción válida.");
+                    continue;
+            }
+            
+        }
+    }
+
+    /** Elimina un artículo de la cesta por código
+     * @method
+     */
+    this.eliminar = function (codigo) {
+        
+        if (this.buscar(codigo) !== false) {
+            this.articulos.splice(this.buscar(codigo), 1);
+            alert("Artículo eliminado con éxito.");
+        }
+        else {
+            alert("Error: Artículo no encontrado");
+        }
+    }
+    /** Vacía la cesta.
+     * @method
+     */
     this.vaciar = function () {
         this.articulos = [];
-        this.mostrar();
+        alert("Se ha vaciado la cesta.");
     }
 }
 
@@ -95,32 +169,39 @@ let cesta = new Cesta(
     new Articulo(124, "Artículo2", 5, 3)
 );
 
-
+/** Abre un menú desplegable con opciones para manejar los datos. */
 function menu() {
     var entrada = 0;
     var salir = false;
-    while (salir == false) {
+    while (salir === false) {
         entrada = prompt(`
         Escoja una opción:
-        1. Ver cesta
-        2. Insertar un articulo
-        3. Borrar un artículo
-        4. Vaciar cesta
-        5. Salir
+        1. Insertar un articulo.
+        2. Editar un artículo.
+        2. Borrar un artículo.
+        3. Vaciar cesta.
+        4. Salir del menú.
         `);
 
         switch (parseInt(entrada)) {
             case 1:
+                cesta.insertar();
                 cesta.mostrar();
                 break;
+
             case 2:
-                cesta.insertar();
+                var codigo = parseInt(prompt("Introduzca el código del artículo que desea editar."));
+                cesta.editar(codigo);
+                cesta.mostrar();
                 break;
             case 3:
-                cesta.eliminar();
+                var codigo = parseInt(prompt("Introduzca el código del artículo que desea eliminar."));
+                cesta.eliminar(codigo);
+                cesta.mostrar();
                 break;
             case 4:
                 cesta.vaciar();
+                cesta.mostrar();
                 break;
             case 5:
                 break;
