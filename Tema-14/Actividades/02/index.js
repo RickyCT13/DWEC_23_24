56972d16d2b575b4ea0e36992d1a2445830d923b
RickyCT13 <ricardomorenocantea13@gmail.com>
2024-02-16 10:47:28 +0100

    16/2/2024 - Progreso Tarea 2 Tema 14

--- Tema-14/Actividades/02/index.js
+++ Tema-14/Actividades/02/index.js
@@ -1,46 +1,71 @@
 /*
     MongoDB: Conexión con Node.js
 */
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import express from 'express';
 import path from 'path';
 
+/*
 const app = express();
 const router = express.Router();
 var __dirname = path.resolve(); //Resuelve y adapta para módulos ES6
-
+*/
 /*
     Conexión al cluster
 */
-const uri = 'mongodb+srv://ricardomorenocantea13:<password>@cluster0.cj2exnu.mongodb.net/?retryWrites=true&w=majority';
+const uri = "mongodb+srv://ricardomorenocantea13:ILIgRjGFH84Nlsvq@cluster0.cj2exnu.mongodb.net/?retryWrites=true&w=majority";
 
-export async function run() {
+
+async function run(fun) {
     const client = new MongoClient(uri);
     try {
         await client.connect();
         const db = client.db('dwec-tema-14');
-        const collection = client.collection('personas');
-        
-        /*var registros = collection.find({}).toArray(function (err, result) {
-            if (err) {
-                console.error(err);
-            }
-            console.log(result);
-        });
+        const collection = db.collection('personas');
 
-        return registros;*/
+        /*
+            Callback que permitirá a otras funciones
+            usar el mongoclient.
+        */
+        await fun(client, db, collection).catch(error => console.error(error));
     }
     finally {
+        /*
+            Se cierra la conexión sin importar el resultado
+        */
         await client.close();
     }
 }
-
+async function getAll() {
+    run(async function (client, db, collection) {
+        /*
+            Utilizamos "let" en vez de "const" para que se actualicen
+            los valores
+        */
+        let queryAll = await collection.find();
+        let allValues = await queryAll.toArray();
+        console.log(allValues);
+    });
+}
+async function create(registro) {
+    run(async function (client, db, collection) {
+        if (registro != null) {
+            await collection.insertOne(registro);
+        }
+    });
+}
+getAll();
+create({
+    nombre: "John",
+    apellido: "PHP"
+});
+getAll();
 
 
 /*
     Al entrar en el directorio raíz, muestra index.html
 */
-router.get('/', function (req, res) {
+/*router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 
 });
@@ -48,13 +73,11 @@ router.get('/', function (req, res) {
 /*
     Manejo de solicitud AJAX
 */
-router.get('/get', function (req, res) {
-    run().catch(error => {
-        console.error(error);
-    });
+/*router.get('/get', function (req, res) {
+    
 });
 
 app.use('/', router);
 app.use(express.static(__dirname));//IMPORTANTE carga archivos js,css, etc.., cargados en los html desde directorio
 app.listen(3000);
-console.log('Escuchando en puerto 3000');
+console.log('Escuchando en puerto 3000');*/
