e2c192369d1118cabb3330a7f49e617d2bc49b21
RickyCT13 <ricardomorenocantea13@gmail.com>
2024-03-14 12:06:07 +0100

    14/3/2024 - Examen entregado

---
+++ Tema-15/Examenes/Final/examen recup final 23_24.ts
@@ -0,0 +1,65 @@
+class Costes{
+        
+    constructor(url:string){
+    
+            this.costesFunction(url); 
+    }
+    async costesFunction(url:string)  {
+        try {
+            //----------------------------
+            // Completar código aquí
+            //----------------------------
+            /*$.post(url, {
+                ID: $("#ID").val()
+            },
+            function(obj) {
+                $("#datosTabla").html('');
+                obj = JSON.parse(obj);
+                obj = obj[0];
+                $.each(obj.costes, function(index, element) {
+                    $("#datosTabla").append(`<tr><th>${obj.costes[index]}</th><td>${obj.costes[element]}</td></tr>`)
+                });
+            });*/
+
+            var destino = url;
+			var data = {
+				ID: $("#ID").val()
+			};
+			const response = await fetch(destino, {
+				method: "POST",
+				body: JSON.stringify(data),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+			const obj = await response.json();
+			$("#datosTabla").html("");
+			obj.foreach((element: any, index: any) => {
+				$("#datosTabla").append(
+					`<tr><th>${element}</th><td>${obj[index]}</td></tr>`
+				);
+			});
+        }catch(error:any){
+            $("#datosTabla").html('El ID introducido no existe');
+            console.log( error.message);
+        } 
+    }
+}
+
+class TablaCostes {
+
+    private url:string;
+
+    constructor(url:string){
+        this.url= url;
+        $('form').on('submit',this.instCost.bind(this));
+      
+    }
+
+    instCost(e:any) {
+        e.preventDefault(); // Para no abandonar la página al enviar formulario.
+        new Costes(this.url) ;     
+    }
+
+}
+new TablaCostes('examen recup final 23_24.php');
